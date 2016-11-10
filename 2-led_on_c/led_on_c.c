c2a3c8de3c312ca978510485b47f4a48420b7789
SourceLInk <limianqi1995@gmail.com>
2016-11-10 16:31:21 +0800

    .c led_on

--- 2-led_on_c/led_on_c.c
+++ 2-led_on_c/led_on_c.c
@@ -3,12 +3,31 @@
 #define GPC0CON	*(volatile unsigned long*)0xE0200060
 #define GPC0DAT *(volatile unsigned long*)0xE0200064 
 
+void delay(int r0)
+{
+	volatile int count = r0;
+	while(count--);
+}
 
 int
 main(void)
 {
-	GPC0CON = 0x01000;	/* 设置GPC0_3 Output */
-	GPC0DAT = 0x08; 	/* Pin state high */
+	/* 初始化io */
+	GPC0CON |= (1 << 12);	/* set GPC0_3 Output */
+	GPC0CON |= (1 << 16);   /* set GPC0_4 Output */
 	
-	while(1);
+	
+	GPC0DAT |= (1 << 3); 	/* set state high */
+	GPC0DAT |= (1 << 4);   	/* set state high */
+
+	while(1) {
+		delay(0x100000);
+	
+		GPC0DAT |= (1 << 4);   	/* set state high */
+		GPC0DAT &= ~(1 << 3);    /* set state low */
+		delay(0x100000);
+	
+		GPC0DAT |= (1 << 3); 	/* set state high */
+		GPC0DAT &= ~(1 << 4);   	/* set state low */
+	}
 }
