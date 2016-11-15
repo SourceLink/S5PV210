b331f0c7202714a12a99f8d7f6f46134d5303d5e
SourceLInk <limianqi1995@gmail.com>
2016-11-15 19:07:02 +0800

    sdram

---
+++ 7-sdram/BL2/led_on_c.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+
+#define GPC0CON	*(volatile unsigned long*)0xE0200060
+#define GPC0DAT *(volatile unsigned long*)0xE0200064 
+
+void delay(int r0)
+{
+	volatile int count = r0;
+	while(count--);
+}
+
+int
+main(void)
+{
+	/* 初始化io */
+	GPC0CON |= (1 << 12);	/* set GPC0_3 Output */
+	GPC0CON |= (1 << 16);   /* set GPC0_4 Output */
+	
+	
+	GPC0DAT |= (1 << 3); 	/* set state high */
+	GPC0DAT |= (1 << 4);   	/* set state high */
+
+	while(1) {
+		delay(0x300000);
+	
+		GPC0DAT |= (1 << 4);   	/* set state high */
+		GPC0DAT &= ~(1 << 3);    /* set state low */
+		delay(0x300000);
+	
+		GPC0DAT |= (1 << 3); 	/* set state high */
+		GPC0DAT &= ~(1 << 4);   	/* set state low */
+	}
+}
